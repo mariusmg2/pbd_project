4b093ba1c6cbd22205e970c53442ded2e1ac22ff
Marius Marusanici <marius.marusanici@gmail.com>
2015-12-08 19:57:31 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$currency = 'RON ';
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'pbd';
+$db_host = 'localhost';
+
+$shipping_cost      = 1.50;
+$taxes              = array(
+                            'Taxa de mediu' => 12,
+                            'Alte taxe' => 5
+                      );
+//connect to MySql
+$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+?>
